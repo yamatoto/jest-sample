3b2733799b59f2b003a9b82505309155e82d5c82
yamatoto <mftb35ymt@yahoo.co.jp>
2021-12-20 23:24:53 +0900

    lint等の設定

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div>sample</div>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Clock from './views/clock/clock';
+
+function App() {
+    return (
+        <div>
+            <h1>sample</h1>
+            <Clock />
+        </div>
+    );
+}
+
+export default App;
